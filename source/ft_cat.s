83aeef39058b96bab78dfa967d345e564893d46b
Julien Hache <julien.hache7@gmail.com>
2019-11-25 16:51:15 +0100

    Fix ft_cat for macho64 version.

--- source/ft_cat.s
+++ source/ft_cat.s
@@ -6,42 +6,51 @@
 			global			_ft_cat
 			extern			_ft_puts
 
+
+%define		BUFFER_SIZE		128
+
+			section			.data
+default rel
+WRONG_FD	db				"ft_cat: no such file or directory", 0 ; same msg than the CLI command
+
+
 			section			.text
+
 _ft_cat:
 ; rdi = PARAM fd
-; r10 is used to save the fd during all the execution of the function
+; r12 is used to save the fd during all the execution of the function
 
 			push			rbp
+			push			r12
 			mov				rbp, rsp
-			sub				rsp, [rel BUFFER_SIZE] ; reserve a few bytes to store the read op
-			mov				r10, rdi ; save rdi in r10
+			sub				rsp, BUFFER_SIZE ; reserve a few bytes to store the read op
+			mov				r12, rdi ; save rdi in r12
 
 read_file:
 ; args for sys_read:
 ;  rdi = unsigned int fd (file descriptor)
-;  rsi = char *buf (buffer to store the read data)
+;  rsi = char *buf (buffer where we store the read data)
 ;  rdx = size_t count (how much byte we want to read)
-;  rax = 0 (not 0 on macOS) && Nb of bytes read.
+;  rax = syscall value && Nb of bytes read.
 
+			mov				rdi, r12
 			mov				rsi, rsp
-			mov				rdi, r10
-			mov				rdx, [rel BUFFER_SIZE]
+			mov				rdx, BUFFER_SIZE
 			mov				rax, 0x2000003 ; syscall read
 			syscall
 
 			cmp				rax, 0
 			jle				read_error
 
-
-write_file:
+;write_file:
 ; args for sys_write:
 ;  rdi = unsigned int fd (1 == standard output)
-;  rsi = const char *buf (our stack pointer)
+;  rsi = const char *buf (buffer where we store the read data)
 ;  rdx = size_t count (take the return value from read)
-;  rax = 1 (not 1 on macOS) && Nb of bytes writed.
+;  rax = syscall value && Nb of bytes writed.
 
+			mov				rdi, 1
 			mov				rsi, rsp
-			mov				rdi, 1 
 			mov				rdx, rax
 			mov				rax, 0x2000004 ; syscall write
 			syscall
@@ -55,16 +64,12 @@ write_file:
 read_error:
 			cmp				rax, 0
 			jz				done ; no error, we just have finish to read the file
-			mov				rdi, WRONG_FD
+			lea				rdi, [WRONG_FD]
 			call			_ft_puts
 
 
 done:
 			mov				rsp, rbp ; restore stack ptr
+			pop				r12
 			pop				rbp
-			;leave
 			ret
-
-			section			.data
-BUFFER_SIZE	dw				128 ; size of the red zone
-WRONG_FD	db				"ft_cat: no such file or directory", 0 ; same msg than the CLI command
